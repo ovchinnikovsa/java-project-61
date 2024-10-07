e1a72cc97b23e5bef398d1205c8819a48db352ac
ovchie <ov4innikovsa@yandex.ru>
2024-10-07 23:59:19 +0300

    beautify

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Cli {
     public static void greeting() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!\n" +
-                "May I have your name?");
+        System.out.println("Welcome to the Brain Games!\n"
+                + "May I have your name?");
         String name = sc.nextLine();
         System.out.println("Hello, " + name + "!");
     }
--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Cli.greeting();
