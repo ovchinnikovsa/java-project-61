e0b73466c35e35de4cb0b30f357ba4163fc01497
ovchie <ov4innikovsa@yandex.ru>
2024-10-11 01:02:48 +0300

    whatAShame

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -3,15 +3,11 @@ package hexlet.code;
 import hexlet.code.app.Cli;
 import hexlet.code.app.exeptions.GameExeption;
 import hexlet.code.app.games.Game;
-import hexlet.code.app.games.calc.CalcOperations;
 import hexlet.code.utils.GameSelect;
 import hexlet.code.utils.TerminalIOMediator;
 
 public class Main {
     public static void main(String[] args) {
-        for (int j = 0; j < 100; j++) {
-            TerminalIOMediator.print(CalcOperations.getRandomOperation().getSymbol());
-        }
         TerminalIOMediator.println("Please enter the game number and press Enter.");
         for (GameSelect option : GameSelect.values()) {
             if (option.getId() == null) {
